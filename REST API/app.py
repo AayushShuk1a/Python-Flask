3e0ea8b52d052a42aacf1c7d8b6c9c2c93ff7e9e
AayushShuk1a <aayush5emails@gmail.com>
2024-08-04 22:26:31 +0530

    REST API using flask

--- REST API/app.py
+++ REST API/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 
 app=Flask(__name__)
 
@@ -6,7 +6,7 @@ app=Flask(__name__)
 stores=[
     {
         "name":"MyStore",
-        "Items":[
+        "items":[
             {
                 "name":"Monitor",
                 "price":20.99
@@ -17,4 +17,41 @@ stores=[
 
 @app.get("/store")
 def getStore():
-    return {"Stores":stores}
+    return {"Stores":stores},200
+
+@app.post("/store")
+def createStore():
+    request_data=request.get_json()
+    newStore={"name":request_data["name"],"items":[]}
+    stores.append(newStore)
+    return newStore,201
+
+@app.post("/store/<string:name>/item")
+def createItems(name):
+    request_data=request.get_json()
+    for store in stores:
+        if store["name"]==name:
+            new_item={
+                "name":request_data["name"],
+                "price":request_data["price"]
+            }
+            store["items"].append(new_item)
+            return new_item,201
+    return "Store Not Found",404
+
+
+@app.get("/store/<string:name>")
+def getSingleStore(name):
+    for store in stores:
+        if store["name"]==name:
+            return store,202
+        
+    return "Store Not Found",404
+
+@app.get("/store/<string:name>/items")
+def getAllItems(name):
+    for store in stores:
+        if store["name"]==name:
+            return {"items":store["items"]},200
+        
+    return "Store Not Found",404
